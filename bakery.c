4ef509ae5550fe3e3cc0c4526c40ca3cf37e7626
JoshusTenakhongva <jtt75@nau.edu>
2019-10-21 15:47:09 -0700

    added baker wait times and batch loop

--- bakery.c
+++ bakery.c
@@ -8,11 +8,6 @@
 
 #define STRING_SIZE 80
 
-// Prototypes
-void* left_baker_work( void* info ); 
-void* right_baker_work( void* info ); 
-void* cautious_baker_work( void* info ); 
-
 // Struct prototype
 typedef struct Thread_info
 	{
@@ -25,7 +20,19 @@ typedef struct Thread_info
 	
 	int leftMitts; 
 	int rightMitts; 
+	
+	
 	} Thread_info; 
+	
+// Enum Prototype
+typedef enum BakerType { left, right, cautious } BakerType; 
+
+// Function Prototypes
+void* left_baker_work( void* info ); 
+void* right_baker_work( void* info ); 
+void* cautious_baker_work( void* info ); 
+int findBakerID( Thread_info* info, BakerType type ); 
+void workWait( int seed ); 
 
 int main( int argc, char* argv[] )
 	{
@@ -133,23 +140,25 @@ void* left_baker_work( void* info )
 	
 	// Initialize variables
 	int baker_id; 
+	int batch; 
+	BakerType bakerType = left; 
 	
 	// Cast our thread info back into a struct
 	Thread_info* thread_info = ( Thread_info* ) info; 
 	
 	// Save our lock
 	pthread_mutex_t* lock = &(thread_info->lock); 
-
-	// Lock the lock while we assign baker IDs
-	pthread_mutex_lock( lock ); 
 	
-	// Save our baker ID
-	baker_id = thread_info->left_baker_count; 
-	printf( "left_baker %d\n", baker_id ); 
-	thread_info->left_baker_count++; 
-
-	// Unlock our lock 
-	pthread_mutex_unlock( lock ); 
+	// Find our Baker's ID
+	baker_id = findBakerID( thread_info, bakerType ); 
+	
+	// Loop through until our baker is done working
+	for( batch = 0; batch < 10; batch++ )
+		{
+		
+		workWait( 0 ); 
+		printf( "left %d batch: %d\n", baker_id, batch );
+		}
 	}
 	
 void* right_baker_work( void* info ) 
@@ -157,23 +166,25 @@ void* right_baker_work( void* info )
 	
 	// Initialize variables
 	int baker_id; 
+	int batch; 
+	BakerType bakerType = right; 
 	
 	// Cast our thread info back into a struct
 	Thread_info* thread_info = ( Thread_info* ) info; 
 	
 	// Save our lock
 	pthread_mutex_t* lock = &(thread_info->lock); 
-
-	// Lock the lock while we assign baker IDs
-	pthread_mutex_lock( lock ); 
 	
-	// Save our baker ID
-	baker_id = thread_info->right_baker_count; 
-	printf( "right_baker %d\n", baker_id ); 
-	thread_info->right_baker_count++; 
-
-	// Unlock our lock 
-	pthread_mutex_unlock( lock ); 
+	// Find our baker's ID
+	baker_id = findBakerID( thread_info, bakerType ); 
+	
+	// Loop through until our baker is done working
+	for( batch = 0; batch < 10; batch++ )
+		{
+		
+		workWait( 0 ); 
+		printf( "right %d batch: %d\n", baker_id, batch );
+		}
 	}
 	
 	
@@ -182,24 +193,96 @@ void* cautious_baker_work( void* info )
 	
 	// Initialize variables
 	int baker_id; 
+	int batch; 
+	BakerType bakerType = cautious; 
 	
 	// Cast our thread info back into a struct
 	Thread_info* thread_info = ( Thread_info* ) info; 
 	
 	// Save our lock
 	pthread_mutex_t* lock = &(thread_info->lock); 
-
+	
+	// Find our baker's ID
+	baker_id = findBakerID( thread_info, bakerType ); 
+	
+	// Loop through until our baker is done working
+	for( batch = 0; batch < 10; batch++ )
+		{
+		
+		workWait( 0 ); 
+		}
+	}
+	
+int findBakerID( Thread_info* info, BakerType type )
+	{
+	
+	int baker_id; 
+	
+	// Save our lock
+	pthread_mutex_t* lock = &( info->lock ); 
+	
 	// Lock the lock while we assign baker IDs
 	pthread_mutex_lock( lock ); 
 	
-	// Save our baker ID
-	baker_id = thread_info->cautious_baker_count; 
-	printf( "cautious_baker %d\n", baker_id ); 
-	thread_info->cautious_baker_count++; 
+	// Save our baker ID based on what bakertype we have
+	if( type == left )
+		{
+		
+		// Save our baker ID
+		baker_id = info->left_baker_count; 
+		printf( "left_baker %d\n", baker_id ); 
+		info->left_baker_count++; 
+		}
+		
+	else if( type == right )
+		{
+		
+		// Save our baker ID
+		baker_id = info->right_baker_count; 
+		printf( "right_baker %d\n", baker_id ); 
+		info->right_baker_count++; 
+		}
+		
+	else
+		{
+		
+		baker_id = info->cautious_baker_count; 
+		printf( "cautious_baker %d\n", baker_id ); 
+		info->cautious_baker_count++; 
+		}
 
 	// Unlock our lock 
 	pthread_mutex_unlock( lock ); 
+	
+	return baker_id; 
 	}
+	
+void workWait( int seed )
+	{
+	
+	int workTime = 3.0 * (double) rand()/ (double)RAND_MAX; 
+	workTime = ( 2 + workTime ) * 100000; 
+	
+	usleep( workTime ); 
+	}
+	
+void retrieveMitts( BakerType type, Thread_info* mittRack )
+	{
+	
+	// Check if the baker needs a left mitt
+	
+		// 
+	
+	// Check if the baker needs a right mitt
+	
+		// 
+		
+	// Check if the baker needs both
+	
+		// 
+	}
+	
+	
 	
 	
 //
